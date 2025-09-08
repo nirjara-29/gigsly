cb6abde0d73bee35bbc92ed602e8cea0a47e6b92
nirjara-29 <nirjaragirnale@gmail.com>
2025-09-08 16:55:58 +0530

    submit solution route added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 import problemsRouter from "./routes/Problems.js";
+import webhookRoute from "./routes/WebhookClerk.js  ";
 import solutionsRouter from "./routes/Solutions.js"
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
 
 // Test route
 app.get("/", (req, res) => res.send("Backend is running 🚀"));
